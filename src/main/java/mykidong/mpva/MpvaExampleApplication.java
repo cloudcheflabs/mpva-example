9fa01cd994d34dd65be1015c381b0bb3cfc080c1
Cloud Chef Labs <admin@cloudchef-labs.com>
2025-09-03 16:15:30 +0900

    .

---
+++ src/main/java/mykidong/mpva/MpvaExampleApplication.java
@@ -0,0 +1,12 @@
+package mykidong.mpva;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MpvaExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MpvaExampleApplication.class, args);
+    }
+}
