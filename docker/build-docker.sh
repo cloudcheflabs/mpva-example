65a88b544e5eefdbbb4a938fc37b906832a60a94
Cloud Chef Labs <admin@cloudchef-labs.com>
2025-09-04 14:13:17 +0900

    helm chart, docker, jenkins file added.

---
+++ docker/build-docker.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+
+
+set -e -x
+
+export APP_JAR=mpva-mpva-example-1.0.0-SNAPSHOT.jar;
+export IMAGE=pdfjst7m.private-ncr.gov-ntruss.com/mpva-example:latest
+
+
+for i in "$@"
+do
+case $i in
+    --jar=*)
+    APP_JAR="${i#*=}"
+    shift
+    ;;
+    --image=*)
+    IMAGE="${i#*=}"
+    shift
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+done
+
+echo "APP_JAR = ${APP_JAR}"
+echo "IMAGE = ${IMAGE}"
+
+
+# move to build directory.
+cd ../
+
+
+# build all.
+./gradlew clean build -x test;
+
+
+# add fat jar to docker directory.
+cp build/libs/$APP_JAR docker;
+
+set +e -x
+
+## remove old docker image.
+IFS=: read -r IMAGE_PREFIX IMAGE_TAG <<< "$IMAGE"
+echo "image prefix: $IMAGE_PREFIX"
+
+docker rmi -f $(docker images -a | grep ${IMAGE_PREFIX} | awk '{print $3}')
+
+set -e -x
+
+## build.
+docker build \
+--build-arg APP_JAR=${APP_JAR} \
+-t ${IMAGE} \
+./docker;
+
+
+## remove fat jar from docker directory.
+rm -rf docker/$APP_JAR;
+
+# push docker image.
+docker push ${IMAGE};
---
+++ docker/run.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -eux;
+
+# run spring boot application.
+java \
+$MPVA_JAVA_OPTS \
+--add-opens jdk.naming.rmi/com.sun.jndi.rmi.registry=ALL-UNNAMED \
+--add-opens java.base/java.lang=ALL-UNNAMED \
+--add-opens java.base/java.util=ALL-UNNAMED \
+--add-opens java.base/sun.nio.ch=ALL-UNNAMED \
+--add-opens java.base/sun.security.action=ALL-UNNAMED \
+--add-opens java.base/sun.net=ALL-UNNAMED \
+-jar \
+*.jar \
+--spring.config.location=file:/opt/mpva/conf/application.yml
